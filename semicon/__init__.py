3b0f048175a64bcdc6fbe0a503aee7aa92ce1e75
Joseph Weston <joseph@weston.cloud>
2018-12-14 14:26:38 +0100

    add 'semicon.test' so users may test their installations
    Given the issues with various version of sympy, this will be useful
    for debugging user errors

--- semicon/__init__.py
+++ semicon/__init__.py
@@ -6,3 +6,13 @@ __all__.append('__version__')
 for module in ['parameters', 'models', 'peierls']:
     exec('from . import {0}'.format(module))
     __all__.append(module)
+
+
+def test(verbose=True):
+    from pytest import main
+    import os.path
+
+    return main([os.path.dirname(os.path.abspath(__file__)),
+                 "-s"] + (['-v'] if verbose else []))
+
+test.__test__ = False
